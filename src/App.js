fa4c50af49cc32cca16fa5f87aa7c1aba895a624
Rishika-Raveendran <rishikarocky14@gmail.com>
2022-01-25 19:14:56 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,30 @@
-
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./App.css";
 
+class App extends Component {
+  state = {
+    num: 0,
+  };
 
-class App extends Component { 
-  
-   state = {    
-    num:0    
-   } 
+  increment = () => {
+    this.setState({ num: this.state.num + 1 });
+  };
 
+  decrement = () => {
+    this.setState({ num: this.state.num - 1 });
+  };
 
-   increment = ()=>{      
-    this.setState({num : this.state.num+1});     
-   }
-
-   decrement = ()=>{
-     this.setState({num: this.state.num-1})
-   }
-
-   
-
-
-
-  render() {  
-
-    function hello(){
-         this.setState({num : this.state.num+1});
-      }
-
-        return (
-    <div className='App'>
-      <div className='showcase'>
-      <h1>{this.state.num}</h1>
-      <button onClick={this.increment}>ADD</button>
-      <button onClick={this.decrement}>SUB</button>
-
+  render() {
+    return (
+      <div className="App">
+        <div className="showcase">
+          <h1>{this.state.num}</h1>
+          <button onClick={this.increment}>ADD</button>
+          <button onClick={this.decrement}>SUB</button>
+        </div>
       </div>
-
-
-    </div>
-    
     );
   }
 }
- 
+
 export default App;
-
-
- 
-// Hooks/Lifecycle hooks
